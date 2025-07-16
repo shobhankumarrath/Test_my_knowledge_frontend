d9583078cbd048add5dfbbe117a046bba36f4378
unknown <Shobhan>
2025-07-17 00:13:00 +0530

    Added base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Test_my_knowledge_frontend/",
+  base: "./",
   plugins: [react()],
 });
